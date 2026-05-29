60a970baf73f1ba4cac2c8d19a18fb02e95e8074
Bhawana <bhawanaahirwar52@gmail.com>
2026-05-30 01:18:51 +0530

    Fix: added root endpoint to resolve '/' error

--- src/main/java/com/nexusHR/controller/HomeController.java
+++ src/main/java/com/nexusHR/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.nexusHR.controller;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "NexusHR is running successfully 🚀";
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui/index.html");
     }
+
 }
