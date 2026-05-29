617bd97f64a669c3f023876548a206102ab5182a
Bhawana <bhawanaahirwar52@gmail.com>
2026-05-30 01:12:39 +0530

    Add HomeController

---
+++ src/main/java/com/nexusHR/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.nexusHR.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "NexusHR is running successfully 🚀";
+    }
+}
